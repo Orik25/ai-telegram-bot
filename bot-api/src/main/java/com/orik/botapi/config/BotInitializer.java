fb0ea0b28292bfd167156adf889a1a7f2f2408ab
Orik25 <orestpilavka@gmail.com>
2023-12-06 23:06:36 +0200

    edit bot

--- bot-api/src/main/java/com/orik/botapi/config/BotInitializer.java
+++ bot-api/src/main/java/com/orik/botapi/config/BotInitializer.java
@@ -6,41 +6,30 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.BotSession;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Slf4j
 @Component
 public class BotInitializer {
     private TelegramBot bot;
-    private boolean isRegistered=false;
+
     @Autowired
     public BotInitializer(TelegramBot bot) {
         this.bot = bot;
     }
 
-
     @EventListener({ContextRefreshedEvent.class})
     public void init() throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-
-        if (!isRegistered) {
-            try {
-                telegramBotsApi.registerBot(bot);
-                isRegistered = true;
-                bot.setRegistered(true);
-            } catch (TelegramApiException e) {
-                log.error("Error occurred: " + e.getMessage());
-            }
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
         }
-    }
 
-    private boolean isBotRegistered() {
-        return bot.isRegistered();
     }
 
 }
