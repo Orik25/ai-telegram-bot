3b5bae7e132ef9cc119502cdc0013df4acb53d3a
Orik25 <orestpilavka@gmail.com>
2023-12-06 22:31:37 +0200

    added async

--- bot-api/src/main/java/com/orik/botapi/config/BotInitializer.java
+++ bot-api/src/main/java/com/orik/botapi/config/BotInitializer.java
@@ -2,17 +2,15 @@ package com.orik.botapi.config;
 
 
 import com.orik.botapi.service.impl.TelegramBot;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
-import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Component
@@ -24,22 +22,16 @@ public class BotInitializer {
         this.bot = bot;
     }
 
-    @PostConstruct
-    public void init() {
-        CompletableFuture.runAsync(() -> {
-            TelegramBotsApi telegramBotsApi = null;
-            try {
-                telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-            } catch (TelegramApiException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                telegramBotsApi.registerBot(bot);
-                log.info("Telegram bot registered successfully.");
-            } catch (TelegramApiException e) {
-                log.error("Error occurred while registering the Telegram bot: " + e.getMessage());
-            }
-        }).join(); // Зачекайте завершення асинхронної реєстрації
+    @Async
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+
     }
 
 }
