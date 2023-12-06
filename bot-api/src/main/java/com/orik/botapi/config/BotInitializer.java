3c3127df3b4ca9184f02fd102c077bf885906bfb
Orik25 <orestpilavka@gmail.com>
2023-12-06 22:56:56 +0200

    edit bot

--- bot-api/src/main/java/com/orik/botapi/config/BotInitializer.java
+++ bot-api/src/main/java/com/orik/botapi/config/BotInitializer.java
@@ -10,13 +10,14 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.BotSession;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Slf4j
 @Component
 public class BotInitializer {
     private TelegramBot bot;
-
+    private boolean isRegistered=false;
     @Autowired
     public BotInitializer(TelegramBot bot) {
         this.bot = bot;
@@ -27,9 +28,10 @@ public class BotInitializer {
     public void init() throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
 
-        if (!isBotRegistered()) {
+        if (!isRegistered) {
             try {
                 telegramBotsApi.registerBot(bot);
+                isRegistered = true;
                 bot.setRegistered(true);
             } catch (TelegramApiException e) {
                 log.error("Error occurred: " + e.getMessage());
